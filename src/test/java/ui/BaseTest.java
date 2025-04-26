f454ca5de4c06d3adf3f4f13557dc2ff24a59906
gchursov <topsycreed@gmail.com>
2025-04-26 15:03:48 +0200

    Add selenium remote

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 
 public class BaseTest {
     WebDriver driver;
@@ -14,7 +19,7 @@ public class BaseTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        driver = initDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.manage().window().maximize();
     }
@@ -23,4 +28,26 @@ public class BaseTest {
     void teardown() {
         driver.quit();
     }
+
+    private WebDriver initDriver() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        if (remoteUrl != null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Add headless mode
+            options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+            options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+            options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            driver = new ChromeDriver();
+        }
+        driver.manage().window().maximize();
+        return driver;
+    }
+
 }
